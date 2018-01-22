55a121b9a1b103da2b0a9bc9523a89956f5bad88
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-01-22 10:30:02 +0700

    Change callback to Promise & add some additional comments in book_route file

--- routes/book_route.js
+++ routes/book_route.js
@@ -13,29 +13,29 @@ router.get('/books', isVerified, (req, res, next) => {
 		.find({})
 		.populate('author')
 		.populate('category')
-		.exec((err, results) => {
-			if (err) {
-				res.json({ success: false, message: 'Failed to show books data, please try again.', results: err });
-			} else {
-				res.status(200).json({ success: true, message: 'All books are available.', results: results });
-			}
+		.then(results => {
+			res.status(200).json({ success: true, message: 'All books are available.', results: results });
+		})
+		.catch(err => {
+			res.json({ success: false, message: 'Failed to show books data, please try again.', results: err });
 		});
 });
 
-// == find book by its id ==
+// == find book by id ==
 router.get('/books/:bookid', isVerified, (req, res, next) => {
 	const bookid = req.params.bookid;
 
+	// find book by the id of book and 
+	// populate the author & category of the book
 	Book
-		.findById({ '_id': req.params.bookid })
+		.findById({ '_id': bookid })
 		.populate('author')
 		.populate('category')
-		.exec((err, book) => {
-			if (err) {
-				res.json({ success: false, message: `Oops, a book with id: ${bookid} cannot be found, please try again.`});
-			} else {
-				res.status(200).json({ success: true, message: `Book with id: ${bookid} is found.`, results: book});
-			}
+		.then(book => {
+			res.status(200).json({ success: true, message: `Book with id: ${bookid} is found.`, results: book });
+		})
+		.catch(err => {
+			res.json({ success: false, message: `Oops, a book with id: ${bookid} cannot be found, please try again.`});
 		});
 });
 
@@ -54,34 +54,46 @@ router.post('/books', isVerified, (req, res, next) => {
 	newBook.pages     = pages;
 	newBook.author    = author;
 	newBook.published = published;
-	newBook.save((err, book) => {
-		if (err) {
-			res.json({ success: false, message: 'New book data cannot be save, please try again.', results: err });
-		} else {
+
+	// insert new book to the database, author & category field is
+	// bounded to their schema objectid
+	newBook
+		.save()
+		.then(book => {
 			res.status(201).json({ success: true, message: 'New book successfully saved.', results: book });
-		}
-	});
+		})
+		.catch(err => {
+			res.json({ success: false, message: 'New book data cannot be save, please try again.', results: err });
+		});
 });
 
 // == edit a spesific book ==
 router.put('/books/:bookid', isVerified, (req, res, next) => {
 	const bookid    = req.params.bookid;
-	const book_name = req.body.book_name;
-	const updatedAt     = Date.Now();
+	const title     = req.body.title;
+	const category  = req.body.category;
+	const pages     = req.body.pages;
+	const author    = req.body.author;
+	const published = req.body.published;
 
 	let updatedBook = new Book();
 
-	updatedBook.book_name = book_name;
-	updatedBook.updatedAt     = updatedAt;
+	updatedBook.title     = title;
+	updatedBook.category  = category;
+	updatedBook.pages     = pages;
+	updatedBook.author    = author;
+	updatedBook.published = published;
+	updatedBook._id       = bookid;
 
+	// To update the book, 'bookid' is absolutely required
+	// and 'runValidators' will help to check the schema
 	Book
-		.findByIdAndUpdate(bookid, updatedBook, {})
-		.exec((err, book) => {
-			if (err) {
-				res.json({ success: false, message: 'Failed to update the book data, please try again.', results: err });
-			} else {
-				res.json({ success: true, message: 'New book\'s data has been saved.', results: book });
-			}
+		.findByIdAndUpdate(bookid, updatedBook, { runValidators: true })
+		.then(updatedBook => {
+			res.json({ success: true, message: 'New book\'s data has been saved.', results: updatedBook });
+		})
+		.catch(err => {
+			res.json({ success: false, message: 'Failed to update the book data, please try again.', results: err });
 		});
 });
 
@@ -89,14 +101,15 @@ router.put('/books/:bookid', isVerified, (req, res, next) => {
 router.delete('/books/:bookid', isVerified, (req, res, next) => {
 	const bookid = req.params.bookid;
 
+	// nothing spesific here, just remove the existing document
+	// by a speficic 'bookid'
 	Book
 		.findByIdAndRemove({ '_id': bookid })
-		.exec((err, book) => {
-			if (err) {
-				res.json({ success: false, message: `A book with id: ${bookid} cannot be found or delete, please try again.`});
-			} else {
-				res.status(204).json({ success: true, message: 'The book successfully deleted.' });
-			}
+		.then(book => {
+			res.status(204).json({ success: true });
+		})
+		.catch(err => {
+			res.json({ success: false, message: `A book with id: ${bookid} cannot be found or delete, please try again.`});
 		});
 });
 
