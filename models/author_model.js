d249d344b68efafb39bb9c1edc8fc0ce47195577
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-01-30 17:46:13 +0700

    Add schema type option 'required'

--- models/author_model.js
+++ models/author_model.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const AuthorSchema = new Schema({
-	fullname: { type: String },
-	email: { type: String }
+	fullname: { type: String, required: true },
+	email: { type: String, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Author', AuthorSchema);
