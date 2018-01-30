2a3c3efb223f01b80342f4b3f8beffa8fc876073
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-01-30 17:46:55 +0700

    Add schema type option 'required'

--- models/category_model.js
+++ models/category_model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const CategorySchema = new Schema({
-	category_name: { type: String }
+	category_name: { type: String, required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Category', CategorySchema);
