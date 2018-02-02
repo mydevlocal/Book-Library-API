e3fd4c3e80777d4c0d8b7a8089634cc8e96a201e
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-02-02 09:48:40 +0700

    Add schema type option 'require'

--- models/book_model.js
+++ models/book_model.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const BookSchema = new Schema({
 	title: { type: String, required: true },
-	category: { type: mongoose.Schema.ObjectId, ref: 'Category' },
+	category: { type: mongoose.Schema.ObjectId, ref: 'Category', required: true },
 	pages: { type: Number },
-	author: { type: mongoose.Schema.ObjectId, ref: 'Author' },
+	author: { type: mongoose.Schema.ObjectId, ref: 'Author', required: true },
 	published: { type: Date, required: true }
 }, { timestamps: true });
 
