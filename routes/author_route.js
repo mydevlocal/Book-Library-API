7523542dc2bd955fa4137625f360663f244161c8
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-02-05 15:20:05 +0700

    Make some changes to follow eslint rules

--- routes/author_route.js
+++ routes/author_route.js
@@ -3,43 +3,45 @@ const express = require('express');
 const router = express.Router();
 
 const isVerified = require('../config/token_verification').TokenVerification;
-const Author     = require('../models/author_model');
+const Author = require('../models/author_model');
 
 // == find all authors ==
 router.get('/authors', isVerified, (req, res, next) => {
-	let offset = req.query.offset ? parseInt(req.query.offset, 10) : null;
-	let limit  = req.query.limit ? parseInt(req.query.limit, 10) : null;
+	const offset = req.query.offset ? parseInt(req.query.offset, 10) : null;
+	const limit = req.query.limit ? parseInt(req.query.limit, 10) : null;
 	let total;
 
 	if (offset !== null) {
 		Author
 			.count({})
-			.then(count => {
+			.then((count) => {
 				// initialize total authors
 				total = count;
 
 				// then return authors data
 				return Author
-								.find({})
-								.sort({ fullname: 1 })
-								.skip(offset)
-								.limit(limit)
-								.exec();
+					.find({})
+					.sort({ fullname: 1 })
+					.skip(offset)
+					.limit(limit)
+					.exec();
 			})
-			.then(results => {
-				res.status(200).json({ success: true, message: 'All authors are available.', results: results, limit: limit, total: total });
+			.then((results) => {
+				res.status(200).json({
+					success: true, message: 'All authors are available.', results, limit, total,
+				});
 			})
-			.catch(err => {
+			.catch((err) => {
 				res.json({ success: false, message: 'Failed to show authors data, please try again.', results: err });
 			});
 	} else {
-		// if there is no query string, get all authors 
+		// if there is no query string, get all authors
 		Author
 			.find({})
-			.then(results => {
-				res.status(200).json({ success: true, message: 'All authors are available.', results: results });
+			.then((results) => {
+				res.status(200).json({ success: true, message: 'All authors are available.', results });
 			})
-			.catch(err => {
+			.catch((err) => {
 				res.json({ success: false, message: 'Failed to show authors data, please try again.', results: err });
 			});
 	}
@@ -47,78 +49,77 @@ router.get('/authors', isVerified, (req, res, next) => {
 
 // == find author by id ==
 router.get('/authors/:authorid', isVerified, (req, res, next) => {
-	const authorid = req.params.authorid;
-
+	const { authorid } = req.params;
 	// find author by the id of author
 	Author
-		.findById({ '_id': authorid })
-		.then(author => {
-			res.status(200).json({ success: true, message: `Author with id: ${authorid} is found.`, results: author});
+		.findById({ _id: authorid })
+		.then((author) => {
+			res.status(200).json({ success: true, message: `Author with id: ${authorid} is found.`, results: author });
 		})
-		.catch(err => {
-			res.json({ success: false, message: `Oops, an author with id: ${authorid} cannot be found, please try again.`});
+		.catch((err) => {
+			res.json({ success: false, message: `Oops, an author with id: ${authorid} cannot be found, please try again.`, error: err });
 		});
 });
 
 // == add new author
 router.post('/authors', isVerified, (req, res, next) => {
-	const fullname = req.body.fullname;
-	const email    = req.body.email;
+	const { fullname } = req.body;
+	const { email } = req.body;
 
-	let newAuthor = new Author();
+	const newAuthor = new Author();
 
 	newAuthor.fullname = fullname;
-	newAuthor.email    = email;
+	newAuthor.email = email;
 
 	// insert new author to the database. No validation? soon :)
 	// i can use client-side validation instead :D
 	newAuthor
 		.save()
-		.then(author => {
+		.then((author) => {
 			res.status(201).json({ success: true, message: 'New author successfully saved.', results: author });
 		})
-		.catch(err => {
+		.catch((err) => {
 			res.json({ success: false, message: 'New author data cannot be save, please try again.', results: err });
 		});
 });
 
 // == edit a spesific author ==
 router.put('/authors/:authorid', isVerified, (req, res, next) => {
-	const authorid  = req.params.authorid;
-	const fullname  = req.body.fullname;
-	const email     = req.body.email;
+	const { authorid } = req.params;
+	const { fullname } = req.body;
+	const { email } = req.body;
 
-	let updatedAuthor = new Author();
+	const updatedAuthor = new Author();
 
-	updatedAuthor.fullname  = fullname;
-	updatedAuthor.email     = email;
-	updatedAuthor._id       = authorid;
+	updatedAuthor.fullname = fullname;
+	updatedAuthor.email = email;
+	updatedAuthor._id = authorid; /* eslint no-underscore-dangle: 0 */
 
 	// To update the author, 'authorid' is absolutely required
 	// and 'runValidators' will help to check the schema
 	Author
 		.findByIdAndUpdate(authorid, updatedAuthor, { runValidators: true })
-		.then(author => {
+		.then(() => {
 			res.json({ success: true, message: 'New author\'s data has been saved.', results: updatedAuthor });
 		})
-		.catch(err => {
-			res.json({ success: false, message: 'Failed to update the author data, please try again.', results: err });	
+		.catch((err) => {
+			res.json({ success: false, message: 'Failed to update the author data, please try again.', results: err });
 		});
 });
 
 // == delete a spesific author ==
 router.delete('/authors/:authorid', isVerified, (req, res, next) => {
-	const authorid = req.params.authorid;
+	const { authorid } = req.params;
 
 	// nothing special here, just remove the existing document
 	// by a spesific 'authorid'
 	Author
-		.findByIdAndRemove({ '_id': authorid })
-		.then(author => {
+		.findByIdAndRemove({ _id: authorid })
+		.then(() => {
 			res.status(204).json({ success: true });
 		})
-		.catch(err => {
-			res.json({ success: false, message: `An author with id: ${authorid} cannot be found or delete, please try again.`});
+		.catch((err) => {
+			res.json({ success: false, message: `An author with id: ${authorid} cannot be found or delete, please try again.`, error: err });
 		});
 });
 
