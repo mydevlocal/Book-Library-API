8b015df82d05026abe3265f1b0eb4508f141aae7
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-02-05 14:50:43 +0700

    Make some changes to follow eslint rules

--- config/token_verification.js
+++ config/token_verification.js
@@ -1,11 +1,12 @@
+/* eslint one-var: ["error", { var: "never" }] */
 const jwt = require('jsonwebtoken');
 
 const opts = require('./options');
 
 exports.TokenVerification = (req, res, next) => {
-	let token
-		, getBearerVal
-		, bearerVal = req.headers['authorization'];
+	let token,
+		getBearerVal;
+	const bearerVal = req.headers.authorization;
 
 	if (bearerVal) {
 		getBearerVal = bearerVal.split(' ');
@@ -19,12 +20,11 @@ exports.TokenVerification = (req, res, next) => {
 		jwt.verify(token, opts.jwtSecret.tokenKey, (err, decoded) => {
 			if (err) {
 				res.status(401).json({ success: false, message: 'Failed to authenticate the token!', error: err });
-			} else {
-				req.decoded = decoded;
-				return next();
 			}
+			req.decoded = decoded;
+			return next();
 		});
 	} else {
 		res.status(403).json({ success: false, message: 'No token provided!' });
 	}
-}
+};
