c9ec5a9d26a99ddb927588b794099abf78a5d9e4
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-02-05 14:45:04 +0700

    Remove bunyan package & make some changes to follow eslint rules

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const express    = require('express');
-const path       = require('path');
-const helmet     = require('helmet');
-const cors       = require('cors');
+const express = require('express');
+const path = require('path');
+const helmet = require('helmet');
+const cors = require('cors');
 const bodyParser = require('body-parser');
-const mongoose   = require('mongoose');
-const bunyan     = require('bunyan');
+const mongoose = require('mongoose');
 
-const User     = require('./routes/user_route');
-const Author   = require('./routes/author_route');
+const User = require('./routes/user_route');
+const Author = require('./routes/author_route');
 const Category = require('./routes/category_route');
-const Book     = require('./routes/book_route');
-const opts     = require('./config/options');
+const Book = require('./routes/book_route');
+const opts = require('./config/options');
 
 const port = process.env.PORT;
 const host = process.env.HOST;
@@ -23,43 +22,17 @@ const app = express();
 // Bind connection info to log file
 mongoose.connect(opts.mongodb.dbURL, opts.mongodb.dbOptions);
 mongoose.Promise = global.Promise;
-const db         = mongoose.connection;
-
-// Set up mongodb connection log info into a file
-let mongoLog = bunyan.createLogger({
-  name: 'mongodb-connection-log',
-  streams: [
-    {
-      level: 'info',
-      path: __dirname + '/logs/mongodb-connection.log'
-    }
-  ]
-});
+const db = mongoose.connection;
 db.on('error', (err) => { throw new Error(err); });
-
-// Set up app log for tracking error and info into file
-let log = bunyan.createLogger({
-  name: 'book-library-log',
-  streams: [
-    {
-      level: 'info',
-      path: __dirname + '/logs/error.log'
-    },
-    {
-      level: 'error',
-      path: __dirname + '/logs/error.log' 
-    }
-  ]
-});
 
 // set up middlewares
 app.use(helmet());
 app.use(cors({
-  origin: true,
-  methods: ['PUT', 'GET', 'POST', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  preflighContinues: true,
-  credentials: true
+	origin: true,
+	methods: ['PUT', 'GET', 'POST', 'DELETE', 'OPTIONS'],
+	allowedHeaders: ['Content-Type', 'Authorization'],
+	preflighContinues: true,
+	credentials: true,
 }));
 
 app.use(bodyParser.json());
@@ -74,24 +47,21 @@ app.use('/api/v1', Book);
 
 // set up error handling
 app.use((req, res, next) => {
-  let err = new Error('No routes Found!');
-  err.status = 404;
-  next(err);
+	const err = new Error('No routes Found!');
+	err.status = 404;
+	next(err);
 });
 
 if (app.get('env') === 'development') {
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.send({ message: err.message, error: err });
-  });
-  db.on('error', (err) => mongoLog.info(err));
-  log.info();
-  log.error();
+	app.use((err, req, res, next) => {
+		res.status(err.status || 500);
+		res.send({ message: err.message, error: err });
+	});
 }
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.send({ message: err.message, error: {} });
+	res.status(err.status || 500);
+	res.send({ message: err.message, error: {} });
 });
 
 // launch the server
