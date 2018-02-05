edac67c04068a8c95c42937ae11f35a7e68faa7d
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-02-05 15:08:19 +0700

    Make some changes to follow eslint rules

--- models/author_model.js
+++ models/author_model.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 const AuthorSchema = new Schema({
 	fullname: { type: String, required: true },
-	email: { type: String, required: true }
+	email: { type: String, required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Author', AuthorSchema);
