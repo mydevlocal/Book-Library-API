82a26b4739ee6ef444a1960beb72f5554d65f951
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-02-05 15:08:36 +0700

    Make some changes to follow eslint rules

--- models/book_model.js
+++ models/book_model.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 const BookSchema = new Schema({
 	title: { type: String, required: true },
 	category: { type: mongoose.Schema.ObjectId, ref: 'Category', required: true },
 	pages: { type: Number },
 	author: { type: mongoose.Schema.ObjectId, ref: 'Author', required: true },
-	published: { type: Date, required: true }
+	published: { type: Date, required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Book', BookSchema);
