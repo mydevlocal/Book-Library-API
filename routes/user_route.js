a971f525da053464dfddc0b20cf7b09e32d82408
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-02-05 15:21:37 +0700

    Make some changes to follow eslint rules & bcrypt async mode to password field

--- routes/user_route.js
+++ routes/user_route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const jwt     = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
@@ -8,65 +8,67 @@ const opts = require('../config/options');
 
 // == signup a user to get a speficic token ==
 router.post('/signup', (req, res, next) => {
-	let username     = req.body.username;
-	let password     = req.body.password;
+	const { username } = req.body;
+	const { password } = req.body;
 
 	User
-		.findOne({ 'username': username })
-		.then(user => {
+		.findOne({ username })
+		.then((user) => {
 			// if user found return immediately, else create a new user
 			if (user) {
 				res.json({ success: false, message: 'Username is already taken' });
 			} else {
-				let createUser = new User();
-
-				// use 'encryptPassword' schema method to hash user password
-				createUser.username  = username;
-				createUser.password  = createUser.encryptPassword(password);
+				const createUser = new User({
+					username,
+					password, // encrypted inside pre hook
+				});
 
 				// insert new user to database
 				createUser
 					.save()
-					.then(result => {
+					.then(() => {
 						res.status(201).json({ success: true, message: 'Successful create a new user.' });
 					})
-					.catch(err => {
-						res.status(500).json({ success: false, message: 'Unable to create new user.' });
+					.catch((err) => {
+						res.status(500).json({ success: false, message: 'Unable to create new user.', error: err });
 					});
-			}			
+			}
 		})
-		.catch(err => {
-			res.status(500).json({ success: false, message: 'Cannot proceed your registration data, please try again.' });
+		.catch((err) => {
+			res.status(500).json({ success: false, message: 'Cannot proceed your registration data, please try again.', error: err });
 		});
 });
 
 // == signin to get a token to use the api ==
 router.post('/signin', (req, res, next) => {
-	let username = req.body.username;
-	let password = req.body.password;
+	const { username } = req.body;
+	const { password } = req.body;
 
 	User
-		.findOne({ 'username': username })
-		.then(user => {
+		.findOne({ username })
+		.then((user) => {
 			// check entered user password
-			if(!user.comparePassword(password)) {
-				res.status(401).json({ success: false, message: 'Password didn\'t match.'});
-			} else {
-				// everything passed, set user id as payload
-				let payload = { id: user._id };
+			user.comparePassword(password, (err, isMatch) => {
+				if (!isMatch) {
+					res.status(401).json({ success: false, message: 'Password didn\'t match.' });
+				} else {
+					/* eslint no-underscore-dangle: 0 */
+					// everything passed, set user id as payload
+					const payload = { id: user._id };
 
-				// sign the token, make it to expiresIn 3 days
-				jwt.sign(payload, opts.jwtSecret.tokenKey, { expiresIn: '3d' }, (err, token) => {
-					if (err) {
-						res.status(401).json({ success: false, message: 'Cannot proceed the signature, please try again', error: err });
-					} else {
-						res.json({ success: true, message: 'Login success, get your token', token: token });
-					}
-				});
-			}			
+					// sign the token, make it to expiresIn 3 days
+					jwt.sign(payload, opts.jwtSecret.tokenKey, { expiresIn: '3d' }, (error, token) => {
+						if (error) {
+							res.status(401).json({ success: false, message: 'Cannot proceed the signature, please try again', error });
+						} else {
+							res.json({ success: true, message: 'Login success, get your token', token });
+						}
+					});
+				}
+			});
 		})
-		.catch(err => {
-			res.status(401).json({ success: false, message: 'Invalid username, please try again.'});
+		.catch((err) => {
+			res.status(401).json({ success: false, message: 'Invalid username, please try again.', error: err });
 		});
 });
 
