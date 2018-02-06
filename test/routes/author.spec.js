d763e24cb2d4926ece757bf90ed96719199ec624
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-02-06 14:35:06 +0700

    Make some changes to follow eslint rules

--- test/routes/author.spec.js
+++ test/routes/author.spec.js
@@ -1,68 +1,57 @@
-'use strict';
+/* global describe, it, before, after,
+mockgoose, mongoose, jwt, opts, supertest, server,
+expect, Author, Category */
 
 // == run testing for author routes ==
-describe('# Testing Author Routes', function() {
+describe('# Testing Author Routes', () => {
 	// == This function will run before test to clear author collection ==
 	let apiKey = null;
-	before(function(done) {
-		mockgoose.prepareStorage().then(function() {
+	before((done) => {
+		mockgoose.prepareStorage().then(() => {
 			mongoose.connect(opts.mongodb.dbURL, opts.mongodb.dbOptions);
 			mongoose.Promise = global.Promise;
-			const db         = mongoose.connection;
-			db.on('error', function(err) { done(err); });
+			const db = mongoose.connection;
+			db.on('error', (err) => { done(err); });
 		});
-		
-		// == empty the author & category collection ==
+
+		// == empty the author collection ==
 		Author.remove({}).exec();
-		Category.remove({}).exec();
-
-		// == because of the execution order of mocha, the category must be created before execute book test ==
-		let category = new Category({
-			category_name: 'Mistery'
-		});
-		category.save();
-
-		// == save temporary author ==
-		Author
-			.count({})
-			.then(function(count) {
-				if (count === 0) {
-					let author = new Author({
-						fullname: 'John Doe',
-						email: 'john@johndoe.com'
-					});
-					author.save();
-				}
-			})
-			.catch(function(err) {
-				done(err);
-			});
 
 		// == generate token for testing ==
-		let payload = { id: 'randomid0989835909' };
+		const payload = { id: 'randomid0989835909' };
 
-		jwt.sign(payload, opts.jwtSecret.tokenKey, function(err, token) {
+		jwt.sign(payload, opts.jwtSecret.tokenKey, (err, token) => {
 			apiKey = token;
-		});	
+		});
 		done();
 	});
 
 	// == after passing all testing block, remove all of the collections from temp memory ==
-	after(function(done) {
-		mockgoose.helper.reset().then(function() {
+	after((done) => {
+		mockgoose.helper.reset().then(() => {
 			done();
 		});
 	});
 
 	// == In this test it's expected a author list ==
-	describe('GET /api/v1/authors', function() {
-		it('returns a list of authors', function(done) {
+	describe('GET /api/v1/authors', () => {
+		before((done) => {
+			const author = new Author({
+				fullname: 'John Doe',
+				email: 'john@johndoe.com',
+			});
+			author.save();
+
+			done();
+		});
+
+		it('returns a list of authors', (done) => {
 			supertest(server)
 				.get('/api/v1/authors')
 				.set('Authorization', apiKey)
 				.expect('Content-Type', /json/)
 				.expect(200)
-				.end(function(err, res) {
+				.end((err, res) => {
 					expect(res.body).to.be.an('object');
 					expect(res.body).to.have.property('success').equal(true);
 					expect(res.body).to.have.property('message').to.be.a('string');
@@ -75,14 +64,14 @@ describe('# Testing Author Routes', function() {
 				});
 		});
 
-		it('returns a list of authors per limit value (query string use case)', function(done) {
+		it('returns a list of authors per limit value (query string use case)', (done) => {
 			supertest(server)
 				.get('/api/v1/authors')
 				.query({ offset: 0, limit: 10 })
 				.set('Authorization', apiKey)
 				.expect('Content-Type', /json/)
 				.expect(200)
-				.end(function(err, res) {
+				.end((err, res) => {
 					expect(res.body).to.be.an('object');
 					expect(res.body).to.have.property('success').equal(true);
 					expect(res.body).to.have.property('message').to.be.a('string');
@@ -97,22 +86,22 @@ describe('# Testing Author Routes', function() {
 	});
 
 	// == It's expected a spesific author ==
-	describe('GET /api/v1/authors/{authorid}', function() {
-		it('returns a spesific author id', function(done) {
+	describe('GET /api/v1/authors/{authorid}', () => {
+		it('returns a spesific author id', (done) => {
 			// == create a fake author ==
-			let author = new Author({
+			const author = new Author({
 				fullname: 'Jenny Doe',
-				email: 'jenny@jennydoe.com'
+				email: 'jenny@jennydoe.com',
 			});
 
 			author
-				.save(function(err, author) {
+				.save((err, data) => {
 					supertest(server)
-						.get(`/api/v1/authors/${author._id}`)
+						.get(`/api/v1/authors/${data._id}`) /* eslint no-underscore-dangle: 0 */
 						.set('Authorization', apiKey)
 						.expect('Content-Type', /json/)
 						.expect(200)
-						.end(function(err, res) {
+						.end((error, res) => {
 							expect(res.body).to.be.an('object');
 							expect(res.body).to.have.property('success').equal(true);
 							expect(res.body).to.have.property('message').to.be.a('string');
@@ -120,44 +109,45 @@ describe('# Testing Author Routes', function() {
 							expect(res.body.results).to.have.property('_id').to.be.a('string').to.have.lengthOf(24);
 							expect(res.body.results).to.have.property('fullname').to.be.a('string');
 							expect(res.body.results).to.have.property('email').to.be.a('string');
-							done(err);
-						});					
+							done(error);
+						});
 				});
 		});
 
 		// == testing for author not found ==
-		it('returns a message authorid not found', function(done) {
-			let fakeAuthor = { _id: '1234' };
+		it('returns a message authorid not found', (done) => {
+			const fakeAuthor = { _id: '1234' };
 
 			supertest(server)
 				.get(`/api/v1/authors/${fakeAuthor._id}`)
 				.set('Authorization', apiKey)
 				.expect('Content-Type', /json/)
 				.expect(200)
-				.end(function(err, res) {
-					expect(res.body).to.be.an('object').that.has.all.keys('success', 'message');
+				.end((err, res) => {
+					expect(res.body).to.be.an('object').that.has.all.keys('success', 'message', 'error');
 					expect(res.body).to.have.property('success').equal(false);
 					expect(res.body).to.have.property('message').to.be.a('string');
+					expect(res.body).to.have.property('error').to.be.an('object');
 					done(err);
 				});
 		});
 	});
 
 	// == Testing the save author expecting status 201 of success ==
-	describe('POST /api/v1/authors', function() {
-		it('saves a new author', function(done) {
-			let newAuthor = {
+	describe('POST /api/v1/authors', () => {
+		it('saves a new author', (done) => {
+			const newAuthor = {
 				fullname: 'Jimmy Doe',
-				email: 'jimmy@jimmydoe.com'
+				email: 'jimmy@jimmydoe.com',
 			};
-			
+
 			supertest(server)
 				.post('/api/v1/authors')
 				.set('Authorization', apiKey)
 				.send(newAuthor)
 				.expect('Content-Type', /json/)
 				.expect(201)
-				.end(function(err, res) {
+				.end((err, res) => {
 					expect(res.body).to.be.an('object');
 					expect(res.body).to.have.property('success').equal(true);
 					expect(res.body).to.have.property('message').to.be.a('string');
@@ -170,10 +160,11 @@ describe('# Testing Author Routes', function() {
 		});
 
 		// == testing for failed to save new author ==
-		it('returns a message failed to save author data', function(done) {
-			let fakeAuthor = {
+		it('returns a message failed to save author data', (done) => {
+			const fakeAuthor = {
 				fullname: 'Fake Doe',
-				emails: 'fake@fakedoe.com'  // wrong field `emails`
+				emails: 'fake@fakedoe.com',
+				// wrong field `emails`
 			};
 
 			supertest(server)
@@ -182,7 +173,7 @@ describe('# Testing Author Routes', function() {
 				.send(fakeAuthor)
 				.expect('Content-Type', /json/)
 				.expect(200)
-				.end(function(err, res) {
+				.end((err, res) => {
 					expect(res.body).to.be.an('object');
 					expect(res.body).to.have.property('success').equal(false);
 					expect(res.body).to.have.property('message').to.be.a('string');
@@ -195,23 +186,23 @@ describe('# Testing Author Routes', function() {
 	});
 
 	// == testing how to update an author ==
-	describe('PUT /api/v1/authors{authorid}', function() {
-		it('update an author', function(done) {
-			let updateAuthor = {
+	describe('PUT /api/v1/authors{authorid}', () => {
+		it('update an author', (done) => {
+			const updateAuthor = {
 				fullname: 'Billy Doe',
 				email: 'billy@billydoe.com',
 			};
 
 			Author
 				.findOne({})
-				.exec(function(err, author) {
+				.exec((err, author) => {
 					supertest(server)
 						.put(`/api/v1/authors/${author._id}`)
 						.set('Authorization', apiKey)
-						.send({...updateAuthor, id: author._id})
+						.send({ ...updateAuthor, id: author._id })
 						.expect('Content-Type', /json/)
 						.expect(200)
-						.end(function(err, res) {
+						.end((error, res) => {
 							expect(res.body).to.be.an('object');
 							expect(res.body).to.have.property('success').equal(true);
 							expect(res.body).to.have.property('message').to.be.a('string');
@@ -219,17 +210,17 @@ describe('# Testing Author Routes', function() {
 							expect(res.body.results).to.have.property('_id').to.be.a('string').to.have.lengthOf(24);
 							expect(res.body.results).to.have.property('fullname').to.be.a('string');
 							expect(res.body.results).to.have.property('email').to.be.a('string');
-							done(err);
+							done(error);
 						});
 				});
 		});
 
 		// == testing for failed to update an author ==
-		it('returns a message failed to update author data', function(done) {
-			let fakeAuthor = {
+		it('returns a message failed to update author data', (done) => {
+			const fakeAuthor = {
 				fullname: 'Fake Doe',
 				email: 'fake@fakedoe.com',
-				_id: 1234
+				_id: 1234,
 			};
 
 			supertest(server)
@@ -238,7 +229,7 @@ describe('# Testing Author Routes', function() {
 				.send(fakeAuthor)
 				.expect('Content-Type', /json/)
 				.expect(200)
-				.end(function(err, res) {
+				.end((err, res) => {
 					expect(res.body).to.be.an('object');
 					expect(res.body).to.have.property('success').equal(false);
 					expect(res.body).to.have.property('message').to.be.a('string');
@@ -252,33 +243,44 @@ describe('# Testing Author Routes', function() {
 	});
 
 	// == testing how to delete an author ==
-	describe('DELETE /api/authors/{authorid}', function() {
-		it('remove an author', function(done) {
+	describe('DELETE /api/authors/{authorid}', () => {
+		before((done) => {
+			// == because of the execution order of mocha,
+			// the category must be created before book test case being execute ==
+			const category = new Category({
+				category_name: 'lolo',
+			});
+			category.save();
+			done();
+		});
+
+		it('remove an author', (done) => {
 			Author
 				.findOne({})
-				.exec(function(err, author) {
+				.exec((err, author) => {
 					supertest(server)
 						.delete(`/api/v1/authors/${author._id}`)
 						.set('Authorization', apiKey)
 						.expect(204)
-						.end(function(err, res) {
+						.end((error, res) => {
 							expect(res.body).to.be.an('object');
-							done(err);
+							done(error);
 						});
-				});			
+				});
 		});
 
-		it('returns a message failed to delete an author', function(done) {
-			let author = { _id: 1234 };
+		it('returns a message failed to delete an author', (done) => {
+			const author = { _id: 1234 };
 			supertest(server)
 				.delete(`/api/v1/authors/${author._id}`)
 				.set('Authorization', apiKey)
-				.end(function(err, res) {
-					expect(res.body).to.be.an('object').that.has.all.keys('success', 'message');
+				.end((err, res) => {
+					expect(res.body).to.be.an('object').that.has.all.keys('success', 'message', 'error');
 					expect(res.body).to.have.property('success').equal(false);
 					expect(res.body).to.have.property('message').to.be.a('string');
+					expect(res.body).to.have.property('error').to.be.an('object');
 					done(err);
-				});	
+				});
 		});
 	});
 });
