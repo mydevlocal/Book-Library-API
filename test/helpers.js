6eb051c649c2a027be018f5946e584c26d8eb078
Muh Ibnu Habil Hanafi <ibnu.habil.h@gmail.com>
2018-02-06 14:30:10 +0700

    Make some changes to follow eslint rules

--- test/helpers.js
+++ test/helpers.js
@@ -1,30 +1,31 @@
-const Mongoose  = require('mongoose').Mongoose;
-const mongoose  = new Mongoose();
-const Mockgoose = require('mockgoose').Mockgoose;
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+const { Mongoose } = require('mongoose');
+const { Mockgoose } = require('mockgoose');
+
+const mongoose = new Mongoose();
 const mockgoose = new Mockgoose(mongoose);
 
-const jwt       = require('jsonwebtoken');
-const chai      = require('chai');
+const jwt = require('jsonwebtoken');
+const chai = require('chai');
 const supertest = require('supertest');
-const expect    = require('chai').expect;
 
-const opts     = require('../config/options');
-const server   = require('../server');
-const Author   = require('../models/author_model');
-const User     = require('../models/user_model');
+const opts = require('../config/options');
+const server = require('../server');
+const Author = require('../models/author_model');
+const User = require('../models/user_model');
 const Category = require('../models/category_model');
-const Book     = require('../models/book_model');
+const Book = require('../models/book_model');
 
 // set up global variable
-global.mongoose  = mongoose;
+global.mongoose = mongoose;
 global.mockgoose = mockgoose;
-global.jwt       = jwt;
+global.jwt = jwt;
 global.supertest = supertest;
-global.chai      = chai;
-global.expect    = chai.expect;
-global.opts      = opts;
-global.server    = server;
-global.Author    = Author;
-global.User      = User;
-global.Category  = Category;
-global.Book      = Book;
+global.chai = chai;
+global.expect = chai.expect;
+global.opts = opts;
+global.server = server;
+global.Author = Author;
+global.User = User;
+global.Category = Category;
+global.Book = Book;
